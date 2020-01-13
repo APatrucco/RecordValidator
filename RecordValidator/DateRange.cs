cd7736bfba0423efae1d27786c7ceca44e2ee300
Alessandro Patrucco <faytheffect@gmail.com>
2020-01-13 12:24:07 -0500

    Add project files.

---
+++ RecordValidator/DateRange.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordValidator
+{
+    public class DateRange
+    {
+        public DateRange(DateTime beginDate, DateTime endDate)
+        {
+            this.beginDate = beginDate;
+            this.endDate = endDate;
+        }
+
+        public DateTime beginDate { get; set; }
+        public DateTime endDate { get; set; }
+
+    }
+}
---
+++ RecordValidator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RecordValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader reader = new StreamReader("C:/users/alex.patrucco/documents/dates.csv");
+
+            var data = reader.ReadLine().Split(',');
+
+            List<DateTime> unsortedDates = new List<DateTime> { };
+
+            foreach (var item in data)
+            {
+                var splitData = item.Split('/');
+                var year = int.Parse(splitData[2]);
+                var month = int.Parse(splitData[0]);
+                var day = int.Parse(splitData[1]);
+
+                unsortedDates.Add(new DateTime(year, month, day));
+            }
+
+            static List<DateRange> SortDates(List<DateTime> unsortedDates)
+            {
+                List<DateRange> sortedDates = new List<DateRange> { };
+                
+                unsortedDates.Sort();
+
+                for (var i = 0; i < unsortedDates.Count; i += 2)
+                {
+                    DateRange pair = new DateRange(unsortedDates[i], unsortedDates[i + 1]);
+                    sortedDates.Add(pair);
+                }
+
+                return sortedDates;
+            }
+
+            List<DateRange> sortedDates = SortDates(unsortedDates);
+
+            foreach (var date in sortedDates)
+            {
+                Console.WriteLine($"Start: {date.beginDate.ToShortDateString()} \t End: {date.endDate.ToShortDateString()}");
+            }
+        }
+    }
+}
