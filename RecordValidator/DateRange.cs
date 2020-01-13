e2225a44ee700dd1a5f03935ddf9f39d87f8eb1a
Alessandro Patrucco <faytheffect@gmail.com>
2020-01-13 12:37:30 -0500

    Moved SortDates method to DateRange Class.

--- RecordValidator/DateRange.cs
+++ RecordValidator/DateRange.cs
@@ -14,5 +14,20 @@ namespace RecordValidator
         public DateTime beginDate { get; set; }
         public DateTime endDate { get; set; }
 
+        public static List<DateRange> SortDates(List<DateTime> unsortedDates)
+        {
+            List<DateRange> sortedDates = new List<DateRange> { };
+
+            unsortedDates.Sort();
+
+            for (var i = 0; i < unsortedDates.Count; i += 2)
+            {
+                DateRange pair = new DateRange(unsortedDates[i], unsortedDates[i + 1]);
+                sortedDates.Add(pair);
+            }
+
+            return sortedDates;
+        }
+
     }
 }
--- RecordValidator/Program.cs
+++ RecordValidator/Program.cs
@@ -25,22 +25,7 @@ namespace RecordValidator
                 unsortedDates.Add(new DateTime(year, month, day));
             }
 
-            static List<DateRange> SortDates(List<DateTime> unsortedDates)
-            {
-                List<DateRange> sortedDates = new List<DateRange> { };
-                
-                unsortedDates.Sort();
-
-                for (var i = 0; i < unsortedDates.Count; i += 2)
-                {
-                    DateRange pair = new DateRange(unsortedDates[i], unsortedDates[i + 1]);
-                    sortedDates.Add(pair);
-                }
-
-                return sortedDates;
-            }
-
-            List<DateRange> sortedDates = SortDates(unsortedDates);
+            List<DateRange> sortedDates = DateRange.SortDates(unsortedDates);
 
             foreach (var date in sortedDates)
             {
